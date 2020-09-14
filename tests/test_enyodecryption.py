de427b4cfbd9e5ae82e9a50de4e9be1d479f011e
Apratim Shukla <apratimshukla6@gmail.com>
2020-09-15 02:35:57 +0530

    Improved documentation for tests

--- tests/test_enyodecryption.py
+++ tests/test_enyodecryption.py
@@ -1,3 +1,6 @@
 from enyo.enyoencryption import EnyoDecryption
 
+# Third parameter is optional partition (by default 2)
 test = EnyoDecryption("SQpaSN","secretkey")
+# To print the decrypted text
+print(test.decrypted)
--- tests/test_enyoencryption.py
+++ tests/test_enyoencryption.py
@@ -1,3 +1,6 @@
 from enyo.enyoencryption import EnyoEncryption
 
+# Third parameter is optional partition (by default 2)
 test = EnyoEncryption("test","secretkey")
+# To print the encrypted text
+print(test.encrypted)
